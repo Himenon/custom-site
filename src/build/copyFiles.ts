76ae9844efbec543ca8755451d401ba275a5b007
Himenon <6715229+Himenon@users.noreply.github.com>
2019-01-01 18:38:57 +0900

    refactor(path): update path calculate and docs

--- src/build/copyFiles.ts
+++ src/build/copyFiles.ts
@@ -6,14 +6,14 @@ const isDirectory = (src: string) => {
   return fsExtra.existsSync(src) && fsExtra.statSync(src).isDirectory();
 };
 
-const isIgnoreExtensions = (src: string, blacklist: CommonOption["blacklist"]) => {
+const isNotBlacklistPattern = (src: string, blacklist: CommonOption["blacklist"]) => {
   return !blacklist.extensions.includes(path.extname(src));
 };
 
 export const copyAssetFiles = async (src: string, dest: string, option: CommonOption) => {
   await fsExtra.copy(src, path.join(dest, option.serverBasePath), {
     filter: (targetPath: string) => {
-      return isDirectory(targetPath) || isIgnoreExtensions(targetPath, option.blacklist);
+      return isDirectory(targetPath) || isNotBlacklistPattern(targetPath, option.blacklist);
     },
     recursive: true,
   });
