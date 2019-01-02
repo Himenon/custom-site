971c9238888c571c9b832fcd4419fb7325858745
Himenon <6715229+Himenon@users.noreply.github.com>
2019-01-02 22:56:10 +0900

    fix(typing): export typing files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export { generateStaticPages } from "./generator";
 export { notifyLog } from "./logger";
 export { server } from "./server";
+/**
+ * Type Files
+ */
+export * from "@rocu/cli";
+export * from "@rocu/development";
+export * from "@rocu/component";
+export * from "@rocu/page";
