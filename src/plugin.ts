f43c7c967728df410e6d07723dacf9c0627d7bf7
Himenon <6715229+Himenon@users.noreply.github.com>
2019-01-21 04:06:21 +0900

    feat(plugin-system): update typings

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,21 +1,19 @@
 import { State as InternalState } from "@custom-site/internal";
 import { Store as InternalStore, store as internalStore } from "./store";
 
-type CreateMap<T> = { [P in keyof T]?: Array<(payload: T[P]) => T[P]> };
-type GetHandler<K> = K extends Array<infer R> ? R : never;
-export type EventHandlerMap = CreateMap<InternalState>;
+type CreateHandlerMap<T> = { [P in keyof T]?: Array<(payload: T[P]) => T[P]> };
+type CreateHandler<K extends keyof EventHandlerMap> = (payload: InternalState[K]) => InternalState[K];
+type EventHandlerMap = CreateHandlerMap<InternalState>;
 
 export const createStore = (iStore: InternalStore) => {
   const handlers: EventHandlerMap = {};
   return {
-    on<K extends keyof EventHandlerMap>(event: K, handler: GetHandler<EventHandlerMap[K]>): void {
-      // @ts-ignore TypeScriptの解析が追いついていない
-      (handlers[event] || (handlers[event] = [])).push(handler);
+    on<K extends keyof EventHandlerMap>(event: K, handler: CreateHandler<K>): void {
+      ((handlers[event] || (handlers[event] = [])) as Array<CreateHandler<K>>).push(handler);
     },
     emit<K extends keyof EventHandlerMap>(event: K, state: InternalState[K] & { id: string }): void {
       let newState = iStore.getState({ type: event, id: state.id }) || state;
-      // @ts-ignore TypeScriptの解析が追いついていない
-      (handlers[event] || (handlers[event] = [])).forEach(handler => {
+      ((handlers[event] || (handlers[event] = [])) as Array<CreateHandler<K>>).forEach(handler => {
         newState = handler(newState);
       });
       iStore.saveState({ type: event, id: state.id, state: newState });
